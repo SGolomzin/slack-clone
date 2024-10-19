486f4f0055fdb38b90775b74ecf0f6fd25fe863b
SGolomzin <s.golomzin@gmail.com>
2024-10-19 19:31:22 +0300

    Create convex API for create channels. Add use-create-channel hook. Implement it inside modal.

--- src/features/channels/components/create-channel-modal.tsx
+++ src/features/channels/components/create-channel-modal.tsx
@@ -11,10 +11,15 @@ import { Input } from "~/components/ui/input";
 import { Button } from "~/components/ui/button";
 
 import { useCreateChannelModal } from "~/features/channels/store/use-create-channel-modal";
+import { useCreateChannel } from "~/features/channels/api/use-create-channel";
+import { useGetWorkspace } from "~/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "~/hooks/use-workspace-id";
 
 
 export const CreateChannelModal = () => {
+	const workspaceId = useWorkspaceId();
 	const [open, setOpen] = useCreateChannelModal();
+	const { mutate, isPending } = useCreateChannel();
 
 	const [name, setName] = useState("");
 
@@ -31,16 +36,29 @@ export const CreateChannelModal = () => {
 		setName(value);
 	};
 
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		mutate(
+			{ name, workspaceId },
+			{
+				onSuccess: (id) => {
+				// 	TODO: Redirect to new channel
+					handleClose();
+				},
+			},
+		);
+	};
+
 	return (
 		<Dialog open={open} onOpenChange={handleClose}>
 			<DialogContent>
 				<DialogHeader>
 					<DialogTitle>Add a channel </DialogTitle>
 				</DialogHeader>
-				<form className="space-y-4">
+				<form className="space-y-4" onSubmit={handleSubmit}>
 					<Input
 						value={name}
-						disabled={false}
+						disabled={isPending}
 						onChange={handleChange}
 						required
 						autoFocus
@@ -49,10 +67,7 @@ export const CreateChannelModal = () => {
 						placeholder="e.g. plan-budget"
 					/>
 					<div className="flex justify-end">
-						<Button
-							disabled={false}
-
-						>
+						<Button disabled={isPending}>
 							Create
 						</Button>
 					</div>
